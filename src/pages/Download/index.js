f75ac89ba91b16effc7a56c5cb61da8bb0103d51
xuejian.xu <jearbilove@yeah.net>
2018-07-07 10:33:50 +0800

    change download files

--- src/pages/Download/index.js
+++ src/pages/Download/index.js
@@ -12,12 +12,13 @@ export default () => {
             <NavBar />
             <div className="main">
                 <div className="title">软件下载</div>
+                <br/>
                 <div className="line">
                     <div className="name">澎博鑫管家-博易大师</div>
                     <button
                         className="btn"
                         onClick={() => {
-                            location.href = 'http://www.qlyzg.com/upload/麒麟博易大师.exe'
+                            location.href = '/upload/博易大师.exe'
                         }}
                     >
                         点击下载
@@ -28,14 +29,25 @@ export default () => {
                     <button
                         className="btn"
                         onClick={() => {
-                            location.href = 'http://www.qlyzg.com/upload/麒麟闪电王.exe'
+                            location.href = '/upload/闪电王.exe'
                         }}
                     >
                         点击下载
                     </button>
                 </div>
                 <div className="line">
-                    <div className="name">手机APP下载</div>
+                    <div className="name">快期</div>
+                    <button
+                        className="btn"
+                        onClick={() => {
+                            location.href = '/upload/快期.exe'
+                        }}
+                    >
+                        点击下载
+                    </button>
+                </div>
+                <div className="line">
+                    <div className="name" style={{verticalAlign: 'top'}}>手机APP下载</div>
                     <img src={imgQR} />
                 </div>
             </div>
